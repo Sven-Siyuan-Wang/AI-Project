5a76f027a674e81f8546f47a38e487c30118c9bd
liusu <liusumy@gmail.com>
2017-07-30 12:59:06 +0800

    implemented upload file

--- app/app.py
+++ app/app.py
@@ -7,21 +7,16 @@ app.config.from_object('config')
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
-
-
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        print f.readline()
         return 'file uploaded successfully'
+
+if __name__ == "__main__":
+    app.run()
 
